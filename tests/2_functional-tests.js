07159b978d9763654a5c9b5e43b3c9a7e0e246a7
codigocaballer <josemariacaballerm@gmail.com>
2022-10-10 22:56:32 +0200

    test: func#5 pressbutton callback final

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -101,7 +101,13 @@ suite('Functional Tests with Zombie.js', function () {
         // #6
         test('Submit the surname "Vespucci" in the HTML form', function (done) {
             browser.fill('surname', 'Vespucci');
-            browser.pressButton('submit', done);
+            browser.pressButton('submit', () => {
+                browser.assert.success();
+                browser.assert.text('span#name', 'Amerigo');
+                browser.assert.text('span#surname', 'Vespucci');
+                browser.assert.elements('span#dates', 1);
+                done();
+            });
         });
     });
 });
